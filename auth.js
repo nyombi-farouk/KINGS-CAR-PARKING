2d875fee5c6d5a653a6d28152b3873da8d287967
nyombi-farouk <trippler33221@gmail.com>
2026-08-04 19:06:04 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,103 @@
-// ===============================
-// KING'S PARKING AUTH SYSTEM
-// LOCAL STORAGE
-// ===============================
+// =================================
+// DEFAULT KING'S PARKING ACCOUNTS
+// =================================
 
+
+function createDefaultAccounts(){
+
+
+let users =
+JSON.parse(
+localStorage.getItem("users")
+)
+||
+[];
+
+
+
+
+// DEFAULT ADMIN ACCOUNT
+
+let adminExists =
+users.find(
+user => user.email === "admin@kingsparking.com"
+);
+
+
+
+if(!adminExists){
+
+
+users.push({
+
+name:"King's Parking Admin",
+
+email:"admin@kingsparking.com",
+
+phone:"0700000000",
+
+password:"admin123",
+
+role:"admin"
+
+
+});
+
+
+}
+
+
+
+
+// DEFAULT DRIVER ACCOUNT
+
+
+let driverExists =
+users.find(
+user => user.email === "driver@kingsparking.com"
+);
+
+
+
+if(!driverExists){
+
+
+users.push({
+
+name:"John Driver",
+
+email:"driver@kingsparking.com",
+
+phone:"0711111111",
+
+password:"driver123",
+
+role:"driver"
+
+
+});
+
+
+}
+
+
+
+
+localStorage.setItem(
+
+"users",
+
+JSON.stringify(users)
+
+);
+
+
+}
+
+
+
+
+createDefaultAccounts();
 
 
 // REGISTER
